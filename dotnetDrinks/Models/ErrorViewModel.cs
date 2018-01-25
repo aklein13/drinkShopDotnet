8161c729e8a887a30884f29e5e66c86e1af1d9f7
Arkadiusz Klein <arek.klein@limebrains.com>
2018-01-25 20:06:41 +0100

    Add project files.

---
+++ dotnetDrinks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetDrinks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
