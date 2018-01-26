31b91515695520729dfc29001fcfd7db922e960c
Arkadiusz Klein <arek.klein@limebrains.com>
2018-01-26 10:35:59 +0100

    Api start page

---
+++ dotnetDrinks/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetDrinks.Controllers
+{
+    [Produces("application/json")]
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        // GET: api
+        [HttpGet]
+        public JsonResult GetApi()
+        {
+            return Json(new { message = "Welcome to Drinks API!", routes = new { drinks = "api/drinks", companies = "api/companies" } });
+        }
+    }
+}
