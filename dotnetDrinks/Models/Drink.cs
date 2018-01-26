9a801646c08feba4eaa9b34744424b3cad1cc7b5
Arkadiusz Klein <arek.klein@limebrains.com>
2018-01-26 10:38:08 +0100

    Clean up

--- dotnetDrinks/Models/Drink.cs
+++ dotnetDrinks/Models/Drink.cs
@@ -1,11 +1,6 @@
 ﻿using dotnetDrinks.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Drinks.Models
 {
